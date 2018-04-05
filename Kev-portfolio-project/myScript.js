51bcc7d873c37017f9953d33dfc166cab1911875
Kevin Zakaria <37377969+efacsen@users.noreply.github.com>
2018-04-05 21:02:18 +0200

    Uploading kev-portfolio-project

---
+++ Kev-portfolio-project/myScript.js
@@ -0,0 +1,95 @@
+
+// Fixed-Navbar
+$(document).ready(function() {
+
+  var toggleAffix = function(affixElement, scrollElement, wrapper) {
+
+    var height = affixElement.outerHeight(),
+        top = wrapper.offset().top;
+
+    if (scrollElement.scrollTop() >= top){
+        wrapper.height(height);
+        affixElement.addClass("affix");
+    }
+    else {
+        affixElement.removeClass("affix");
+        wrapper.height('auto');
+    }
+  };
+  $('[data-toggle="affix"]').each(function() {
+    var ele = $(this),
+        wrapper = $('<div></div>');
+
+    ele.before(wrapper);
+    $(window).on('scroll resize', function() {
+        toggleAffix(ele, $(this), wrapper);
+    });
+
+    // init
+    toggleAffix(ele, $(window), wrapper);
+  });
+
+});
+
+// Smooth Scrolling
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+
+
+/*$(document).ready(function() {
+
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the
+      //nav bar to stick.
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 952) {
+      $('#navScript').addClass('navbar-follow');
+    }
+    if ($(window).scrollTop() < 953) {
+      $('#navScript').removeClass('navbar-follow');
+    }
+  });
+});
+*/
